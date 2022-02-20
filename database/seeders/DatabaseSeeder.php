69939cc1f174b767859f5cc4803adf8dfe157ec2
abdulkaeum <abdul.kaeum@hotmail.com>
2022-02-20 16:32:58 +0000

    create seeder to test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(20)->create();
+
+        for ($i = 1; $i <= 60; $i++) {
+
+            $game = Game::factory()->count(1)->create();
+
+            $score_one = rand(10, 80);
+            $score_two = rand(10, 80);
+
+            $game[0]->users()->attach([
+                rand(1, 10) => [
+                    'score' => $score_one,
+                    'winner' => $score_one > $score_two,
+                    'game_id' => $game[0]->id
+                ],
+                rand(11, 20) => [
+                    'score' => $score_two,
+                    'winner' => $score_two > $score_one,
+                    'game_id' => $game[0]->id
+                ]
+            ]);
+        }
     }
 }
